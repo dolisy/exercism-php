0081aba99dd4c02192b82a5a443a05b1d479b3c9
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-10 16:07:52 +0200

    hello world done

--- hello-world/hello-world.php
+++ hello-world/hello-world.php
@@ -1,13 +1,7 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hello World" exercise.
-// It's been provided as a convenience to get you started writing code faster.
-//
-
-function helloWorld()
-{
-    //
-    // YOUR CODE GOES HERE
-    //
+function helloWorld() {
+  return "Hello, World!";
 }
+
+echo helloWorld();
