e023fc53db8a0fadd2ca55b0946721614c7353da
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-10-10 16:20:05 +0200

    hamming done

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -7,7 +7,17 @@
 
 function distance($a, $b)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+  if (strlen($a) != strlen($b)) {
+    throw new InvalidArgumentException('DNA strands must be of equal length.');
+  } else {
+    $distance = 0;
+
+    for ($i = 0; $i < strlen($a); $i++) {
+      if ($a[$i] != $b[$i]) {
+        $distance += 1;
+      }
+    }
+
+    return $distance;
+  }
 }
